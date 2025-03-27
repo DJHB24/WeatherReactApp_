ee7a4ae9667bd7f2b971ccaf2c14eb72829b94a2
Danielle <daniellejjhouser@gmail.com>
2025-03-27 12:14:16 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,40 @@
+ import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://portfolionewdhb.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Danielle
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/DJHB24/WeatherReactApp_"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://portfolionewdhb.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
